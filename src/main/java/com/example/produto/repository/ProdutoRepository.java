d5f16f20007eaa9fa3ffdf6bdedefccfee55e4e9
Letícia Borges <99838964+letiborges2510@users.noreply.github.com>
2026-04-26 03:47:22 -0300

    Delete src directory

--- src/main/java/com/example/produto/repository/ProdutoRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.produto.repository;
-
-import com.example.produto.model.Produto;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface ProdutoRepository extends MongoRepository<Produto, String> {
-
-}
